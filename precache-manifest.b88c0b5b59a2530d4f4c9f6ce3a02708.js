cc51a00cf0a387c3a80e301b4195a9e0375efa5a
Shivam Sharma <shivam15951@gmail.com>
2020-06-18 18:11:33 +0530

    Updates

---
+++ precache-manifest.b88c0b5b59a2530d4f4c9f6ce3a02708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38259daf4154cca3fba345e51b846b00",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "df0e033de5f005251911",
+    "url": "/Covid19-Tracker/static/css/main.dee8ea7d.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/Covid19-Tracker/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid19-Tracker/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0e033de5f005251911",
+    "url": "/Covid19-Tracker/static/js/main.6263f9bf.chunk.js"
+  },
+  {
+    "revision": "cadda132e1f500032829",
+    "url": "/Covid19-Tracker/static/js/runtime-main.aedc54c6.js"
+  }
+]);
